bdbe41fbbe5a199bb62c217dde36174bc3aed705
yuhao.zhang <zyh624215687@gmail.com>
2019-04-02 23:07:53 +0800

    remove https

--- src/utils/setBaseUrl.js
+++ src/utils/setBaseUrl.js
@@ -5,10 +5,10 @@ let urlParams = {
 console.log('process.env.VUE_APP_ENV_ALIAS:'+process.env.VUE_APP_ENV_ALIAS);
 switch (process.env.VUE_APP_ENV_ALIAS) {
     case 'test':
-        urlParams.baseUrl = "https://35.198.235.4:8080/"
+        urlParams.baseUrl = "http://35.198.235.4:8080/"
         break
     case 'production':
-        urlParams.baseUrl = "https://35.198.235.4:8080/"
+        urlParams.baseUrl = "http://35.198.235.4:8080/"
         break
     default:
         urlParams.baseUrl = '/localApi'
