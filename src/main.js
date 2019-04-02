3db11ac125d029981971510fa900b097cb1c1e97
xueyin.gao <358751511@>
2019-04-02 10:58:42 +0800

    【提交原因】dynamic environment variable

--- src/main.js
+++ src/main.js
@@ -12,6 +12,29 @@ import '@/common/constant';
 
 Vue.prototype.$publicPath = process.env.BASE_URL;
 
+
+// Start support for ENV vars
+let CONFIG = !!window.CONFIG ? window.CONFIG : process.env
+function getConfig(k, dfl) {
+    if (CONFIG[k]) {
+        return CONFIG[k]
+    } else if (CONFIG['VUE_APP_'+k]) {
+        return CONFIG['VUE_APP_'+k]
+    } else {
+        return dfl
+    }
+}
+const envPlugin = {
+  install(Vue, options) {
+      Vue.prototype.$env = {
+          get: function (k, dfl) {
+              return getConfig(k, dfl)
+          }
+      }
+  }
+}
+Vue.use(envPlugin)
+
 new Vue({
   router,
   store,
